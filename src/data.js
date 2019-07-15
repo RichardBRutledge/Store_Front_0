b78a7cf627349f896fc3de0afed711f6a4fd5a82
RichardBRutledge <36484605+RichardBRutledge@users.noreply.github.com>
2019-07-15 15:45:16 -0500

    Added another product.

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
 export const storeProducts = [
 {
     id: 1,
-        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg1.mashed.com%2Fimg%2Fgallery%2Freasons-to-make-cabbage-your-new-go-to-food%2Fintro.jpg&imgrefurl=https%3A%2F%2Fwww.mashed.com%2F21692%2Freasons-make-cabbage-new-go-food%2F&docid=ZmGb5lataVXDfM&tbnid=gU9oRzeqbNuzeM%3A&vet=10ahUKEwjDi6f08ofjAhUJS6wKHRxeA1QQMwikASgZMBk..i&w=780&h=439&bih=963&biw=958&q=cabbage&ved=0ahUKEwjDi6f08ofjAhUJS6wKHRxeA1QQMwikASgZMBk&iact=mrc&uact=8",
+        img: "https://www.foodsforbetterhealth.com/wp-content/uploads/2017/02/cabbage-750x400.jpg",
     title: "Cabbage",
-    description: "This is a cabbage.",
+    description: "This is cabbage.",
     Price: 4,
     inCart: false,
     count: 0,
@@ -14,18 +14,40 @@ export const storeProducts = [
     id: 2,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxDvkGKwAGNrtG3h7_pKBiqrF9w49bYCszGn_2EXdxeSwY8iiSyg",
     title: "Apple",
-    description: "this is apple",
+    description: "This is apple",
     Price: 10,
     inCart: false,
     count: 0,
     total: 0
 },
 
+{
+    id: 3,
+    img: 'https://cosmos-images2.imgix.net/file/spina/photo/13954/100118_Debunked_01.jpg?ixlib=rails-2.1.4&auto=format&ch=Width%2CDPR&fit=max&w=835',
+    title: 'Bannana',
+    description: "This is bannana",
+    Price: 10,
+    inCart: false,
+    count: 0,
+    total: 0
+},
+
+{
+    id: 4,
+    img: 'http://www.pickolor.com/images/main/meaning-orange-fruit.jpg',
+    title: 'orange',
+    description: 'This is orange',
+    price: 10,
+    inCart: false,
+    count: 0,
+    total: 0
+}
+
 ];
 
 export const detailProduct = {
     id: 1,
-    title:"",
+    title:"This product",
     img: "",
     price: 10,
     inCart: false,
