f96ab011c1636b2cfb9d21fa11916952d798c7c3
RichardBRutledge <36484605+RichardBRutledge@users.noreply.github.com>
2019-07-15 15:47:23 -0500

    Changing bootstrap to around.

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,24 +7,27 @@ export default class Product extends Component {
     render() {
         const { id, description, title, img, price, inCart } = this.props.product;
         return (
-            <div className='col-9 mx-auto col-lg col-md-6'>
+            <div className='col-9 mx-auto md-6 lg-3 my-2'>
                 <div className='card'>
                     <div className='img-container p-3' onClick={console.log('clicked on my image')}>
                         <Link to='/details'>
-                            <img src={img} alt='product' className='card-img-top' />
+                            <img src={img}  alt='product' className='image-fluid card-img-top' />
                         </Link>
                     </div>
+                    <div className='pb-0 px-3'>
                     <h1>{title}</h1>
                     <p>{description}</p>
+                    </div>
+                    {inCart ? (<p className='text-capitalize mb-0' disabled>In Cart</p>) : (<i className='fas fa-cart-plus' />)}
                     <button
-                        className='btn btn-dark btn-sm'
+                        className='cart-btn'
                         disabled={inCart ? true : false}
                         onClick={() => {
                             console.log('added to cart');
-                        }}>
-                        Check it out!
+                        }}
+                    >
+                        add to cart
                     </button>
-                    {inCart ? (<p className='text-capitalize mb-0' disabled>In Cart</p>) : (<i className='fas fa-cart-plus' />)}
                 </div>
             </div>
         )
