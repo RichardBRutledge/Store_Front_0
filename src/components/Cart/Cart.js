e5414a5905298d72503bf158cb8cc54a2a8a3f0d
RichardBRutledge <36484605+RichardBRutledge@users.noreply.github.com>
2019-09-19 00:15:39 -0500

    paypal implemented

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Head } from '../Head'
 import CartColums from './CartColums'
 import EmptyCart from './EmptyCart'
@@ -6,31 +6,36 @@ import { ProductConsumer, ProductProvider } from '../../Context'
 import CartList from './CartList'
 import CartTotal from './CartTotals'
 
-const Cart = () => {
-    return (
-        <div>
-            <ProductConsumer>
-                {value => {
-                    const { cart } = value;
-                    console.log(value, cart)
-                    if (cart.length > 0) {
-                        return (
-                            <>
-                                <Head name='Your' title='Cart' />
-                                <CartColums />
-                                <CartList value={value} />
-                                <CartTotal value={value} />
-                            </>
-                        );
-                    } else {
-                       return <EmptyCart />;
-                    }
-                }}
-            </ProductConsumer>
-            
-        </div>
-    )
+export default class Cart extends Component {
+    render() {
+        return (
+            <div>
+                <ProductConsumer>
+                    {value => {
+                        const { cart } = value;
+                        console.log(value, cart)
+                        if (cart.length > 0) {
+                            return (
+                                <>
+                                    <Head name='Your' title='Cart' />
+                                    <CartColums />
+                                    <CartList value={value} />
+                                    <CartTotal 
+                                        value={value} 
+                                        history={this.props.history} 
+                                    />
+                                </>
+                            );
+                        } else {
+                            return <EmptyCart />;
+                        }
+                    }}
+                </ProductConsumer>
+
+            </div>
+        )
+    }
+    
 }
 
-export default Cart
 
--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PayPalButton from './PayPalButton';
 
-const CartTotals = ({ value }) => {
+const CartTotals = ({ value, history }) => {
     const { cartSubTotal, cartTax, cartTotal, clearCart, id } = value;
     return (
         <div className='container'>
@@ -34,6 +35,11 @@ const CartTotals = ({ value }) => {
                             <strong>$ { cartTotal }</strong>
                         </span>
                     </h5>
+                    <PayPalButton 
+                        total={cartTotal}
+                        clearCart={clearCart}
+                        history={history}
+                    />
                 </div>
             </div>
         </div>
